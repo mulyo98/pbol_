db5ab821a04ca895017f107f6dccb859490fc2f5
mulyo <mulyo@DESKTOP-GDTRE1P>
2018-12-17 01:35:02 +0700

    chatting

---
+++ src/TugasChatting/Chatting.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasChatting;
+
+
+ 
+public class Chatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Server s = new Server();
+        s.setVisible(true);
+        // TODO code application logic here
+    }
+}
